3aee6f12a025e49f791fcf96167624df47273418
Khel <torres.michael36@gmail.com>
2026-05-29 22:10:12 +0800

    add deployment script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',          // Tells Next.js to generate the 'out' folder
+  basePath: '/khel-portfolio', // Must match your GitHub repository name
+  images: {
+    unoptimized: true,       // Next.js default image optimization won't work on static hosting
+  },
 };
 
 export default nextConfig;
